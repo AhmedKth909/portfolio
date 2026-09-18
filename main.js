dbbaa04d2228e6131184551d8819ec96dfcea819
AhmedKth909 <ahmedkalthom977@gmail.com>
2026-09-18 17:12:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -149,6 +149,7 @@ const galleryData = {
         subtitle: 'مجموعة مختارة من أفضل أعمال التصميم الجرافيكي',
         categories: ['الكل', 'العروض التقديمية', 'حملات وسائل التواصل الاجتماعي', 'الكاروسيل', 'الإعلانات + تصاميم', 'غلافات Reels'],
         items: [
+             { title: ' تصميم الفوز بـ WRO 2026 ', category: 'الإعلانات + تصاميم', description: ' تصميم بوستر الفوز WRO 2026 ', type: 'image', src: './assets/design/الاحتفال.webp',thumbnail: './assets/design/الاحتفال.webp' },
             { title: ' تصميم إعلان بروتين', category: 'الإعلانات + تصاميم', description: ' تصميم إعلان لمنتج بروتين بدون عنصر بشري ', type: 'image', src: './assets/design/muscle.webp',thumbnail: './assets/design/muscle.webp' },
             { title: '  تصميم متطوع الشهر', category: 'الإعلانات + تصاميم', description: ' تصميم متطوع الشهر لفريق الهمك للذكاء الاصطناعي و الروبوتيك ', type: 'image', src: './assets/design/m0.webp',thumbnail: './assets/design/m0.webp' },
             { title: 'تصميم إعلان ورشة تفاعلية ', category: 'الإعلانات + تصاميم', description: ' تصميم إعلان ورشة لفريق الهمك للذكاء الاصطناعي و الروبوتيك اقدمها أنا مدير الميديا والتسويق', type: 'image', src: './assets/design/adwork.webp',thumbnail: './assets/design/adwork.webp' },
